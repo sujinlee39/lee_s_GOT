0b28c65c63db92424038e9b46ac34baf2604948a
Sujin Lee <sujinlee@sujins-mbp-2.fc.ca>
2018-03-21 15:50:36 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 (() => {
   console.log('video stuff fired');
+
+  const vidPlayer = document.querySelector('video').
+        playButton = document.querySelectorAll('button')[0],
+        pauseButton = document.querySelectorAll('button')[1],
+        rewindButton = document.querySelectorAll('button')[2];
+
+  function volOn() {
+    vidPlayer.muted = false;
+  }
+
+  function volOff() {
+    vidPlayer.muted = true;
+  }
+
+  function rewindVid() {
+    vidPlayer.currentTime -= 5;
+  }
+
+  function playVideo() {
+    vidPlayer.play();
+  }
+
+  function pauseVideo() {
+    vidPlayer.pause();
+  }
+
+  vidPlayer.addEventListener('mouseover', volOn);
+  vidPlayer.addEventListener('mouseout', volOff);
+
+  rewindButton.addEventListener('click', rewindVid);
+  pauseButton.addEventListener('click', pauseVideo);
+  playButton.addEventListener('click', playVideo);
 })();
